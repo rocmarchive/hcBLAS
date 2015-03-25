1ddeba6cf4e2941c61ce18f4dc956218dca55dd5
Murugan Vairavel <murugan@multicorewareinc.com>
2015-03-25 20:47:04 +0530

    Modify build.sh to include CMAKE_CXX_FLAG

--- Build/linux/build.sh
+++ Build/linux/build.sh
@@ -1,4 +1,4 @@
 
   
 
-cmake ../../source -DCMAKE_C_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang++
+cmake ../../source -DCMAKE_C_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
