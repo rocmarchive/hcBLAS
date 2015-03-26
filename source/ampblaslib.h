39960bf4ebdcfe11fbd54db1328961916172ae78
Meena Karunanidhi <meena@multicorewareinc.com>
2015-03-26 20:16:14 +0530

    [Development] Add SGER C++ AMP Routines

--- source/ampblaslib.h
+++ source/ampblaslib.h
@@ -59,22 +59,23 @@ class Ampblaslibrary
                                  float *Y, const int incY, long xOffset,
                                  long yOffset);
 
-/*                  A = alpha * X * Y' + A                             */
-    ampblasStatus  ampblas_sger(const enum AMPBLAS_ORDER order, const int M, const int N,
-                                const float alpha, const float *X, const int incX,
-                                const float *Y, const int incY, float *A, const int lda);
+/*                  A = alpha * X * Y' + A                               */
+    ampblasStatus ampblas_sger(const int M, const int N, const float *alpha,
+                               float *X, const long xOffset, const int incX,
+                               float *Y, const long yOffset, const int incY,
+                               float *A, const long aOffset, const int lda);
 
-/*                  Y = alpha * op(A) * X + beta * Y            */
+/*                  Y = alpha * op(A) * X + beta * Y                     */
     ampblasStatus  ampblas_sgemv(const enum AMPBLAS_ORDER order, const enum AMPBLAS_TRANS type, const int M, const int N,
                                  const float *alpha, const float *A, const int lda, const float *X, const int incX,
                                  const float *beta, float *Y, const int incY);
 
-/*                  C = alpha * op(A) * op(B) + beta * C     */
+/*                  C = alpha * op(A) * op(B) + beta * C                 */
     ampblasStatus  ampblas_sgemm(const enum AMPBLAS_ORDER order, const enum AMPBLAS_TRANS typeA, const enum AMPBLAS_TRANS typeB,
                                  const int M, const int N, const int K, const float *alpha, const float *A, const int lda, const float *B, const int ldb ,
                                  const float *beta, float *C, const int ldc);
 
-/*                  C = alpha * op(A) * op(B) + beta * C      */
+/*                  C = alpha * op(A) * op(B) + beta * C                  */
     ampblasStatus  ampblas_cgemm(const enum AMPBLAS_ORDER order, const enum AMPBLAS_TRANS typeA, const enum AMPBLAS_TRANS typeB,
                                  const int M, const int N, const int K, const Complex<float> *alpha, const Complex<float> *A, const int lda, const Complex<float> *B, const int ldb ,
                                  const Complex<float> *beta, Complex<float> *C, const int ldc);
--- source/sger.cpp
+++ source/sger.cpp
@@ -1,9 +1,46 @@
 #include "ampblaslib.h"
+#include <amp.h>
 
-ampblasStatus Ampblaslibrary :: ampblas_sger(const enum AMPBLAS_ORDER order, const int M, const int N,
-                              const float alpha, const float *X, const int incX,
-                              const float *Y, const int incY, float *A, const int lda)
+using namespace concurrency;
+
+void ger_AMP(long m, long n, float alpha,
+             Concurrency::array_view<float> &x, long xOffset, long incx,
+             Concurrency::array_view<float> &y, long yOffset, long incy,
+             Concurrency::array_view<float> &a, long aOffset, long lda)
 {
-    cout << "\nInside ampblas_sger function\n"<<endl;
+  long M = (m + 15) & ~15;
+  long N = (n + 15) & ~15;
+  Concurrency::extent<2> compute_domain(M, N);
+  Concurrency::parallel_for_each(compute_domain.tile<16, 16>(),[=] (Concurrency::tiled_index<16, 16> tidx) restrict(amp)
+  {
+    int i = tidx.global[0];
+    int j = tidx.global[1];
+    if(i < m && j < n)
+      a[aOffset + j*m + i] += x[xOffset + i] * y[yOffset + j] * alpha;
+  });
+}
+
+ampblasStatus Ampblaslibrary :: ampblas_sger(const int M, const int N,
+                                             const float *alpha, float *X,
+                                             const long xOffset, const int incX,
+                                             float *Y, const long yOffset,
+                                             const int incY, float *A,
+                                             const long aOffset, const int lda)
+{
+    long lenX = 1 + (N - 1) * abs(incX);
+    long lenY = 1 + (M - 1) * abs(incY);
+
+    array_view<float> xView(lenX, X);
+    array_view<float> yView(lenY, Y);
+    array_view<float> aMat(M*N, A);
+    
+    ger_AMP(M, N, *alpha, xView, xOffset, incX, yView, yOffset, incY, aMat, aOffset, M);
+    aMat.synchronize();
+
+    /* Debug purpose */
+    /*for (int i = 0 ; i < M * N ;i++ )
+      cout<< "A[" << i << "]" << A[i] << "\n" << endl; 
+    */
+
     return AMPBLAS_SUCCESS;
 }
