22e66b2be379de19f67fea3cf0895556466aa431
Meena <meena@multicorewareinc.com>
2015-04-30 17:35:53 +0530

    Add Software Prefetch Mechanism for TransAB -loop Unroll

--- source/sgemm.cpp
+++ source/sgemm.cpp
@@ -282,6 +282,78 @@ static void gemm_NoTransB_loopunroll_swprefetch(Concurrency::array_view<float, 1
  });
 }
 
+static void gemm_TransAB_loopunroll_swprefetch(Concurrency::array_view<float, 1> &A, long aOffset,
+                                               Concurrency::array_view<float, 1> &B, long bOffset,
+                                               Concurrency::array_view<float, 1> &C, long cOffset,
+                                               int M, int N, int K, int lda, int ldb, int ldc,
+                                               float alpha, float beta)
+{
+  Concurrency::extent<2> grdExt((M + (THREADS - 1)) & ~(THREADS - 1), (N + (THREADS - 1)) & ~(THREADS - 1));
+  Concurrency::tiled_extent<THREADS, THREADS> t_ext(grdExt);
+  Concurrency::array_view<float,2> Cmat = C.view_as<2>(Concurrency::extent<2>(N, M));
+  Concurrency::array_view<float,2> Amat = A.view_as<2>(Concurrency::extent<2>(M, K));
+  Concurrency::array_view<float,2> Bmat = B.view_as<2>(Concurrency::extent<2>(K, N));
+
+  Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<THREADS, THREADS> tidx) restrict(amp)
+  {
+    float CValue = 0;
+    tile_static float As[2][TILE_DIM][TILE_DIM];
+    tile_static float Bs[2][TILE_DIM][TILE_DIM];
+    int kSize = ((K + (TILE_DIM - 1)) & ~(TILE_DIM - 1));
+    int kNext = 0, kmod = 0;
+
+    if (tidx.local[0] < K && tidx.global[1] < N)
+     Bs[0][tidx.local[1]][tidx.local[0]] = Bmat[bOffset + tidx.local[0]][tidx.global[1]];
+    else
+     Bs[0][tidx.local[1]][tidx.local[0]] = 0.0;
+    if (tidx.global[0] < M && tidx.local[1] < K)
+     As[0][tidx.local[0]][tidx.local[1]] = Amat[tidx.global[0]][tidx.local[1]];
+    else
+     As[0][tidx.local[0]][tidx.local[1]] = 0.0;
+
+    for (int k = 0; k < kSize ; k++)
+    {
+      kNext = k + 1;
+      kmod = k & 1;
+
+      if (kNext * TILE_DIM + tidx.local[0] < K && tidx.global[1] < N)
+        Bs[kNext & 1][tidx.local[1]][tidx.local[0]] = Bmat[bOffset + kNext * TILE_DIM + tidx.local[0]][tidx.global[1]];
+      else
+        Bs[kNext & 1][tidx.local[1]][tidx.local[0]] = 0.0;
+      if (tidx.global[0] < M && (kNext * TILE_DIM + tidx.local[1]) < K)
+        As[kNext & 1][tidx.local[0]][tidx.local[1]] = Amat[tidx.global[0]][(kNext * TILE_DIM + tidx.local[1])];
+      else
+        As[kNext & 1][tidx.local[0]][tidx.local[1]] = 0.0;
+
+      tidx.barrier.wait();
+      // Unrolled Matrix Mul operation
+      CValue += Bs[kmod][tidx.local[1]][0] * As[kmod][tidx.local[0]][0] +
+                Bs[kmod][tidx.local[1]][1] * As[kmod][tidx.local[0]][1] +
+                Bs[kmod][tidx.local[1]][2] * As[kmod][tidx.local[0]][2] +
+                Bs[kmod][tidx.local[1]][3] * As[kmod][tidx.local[0]][3] +
+                Bs[kmod][tidx.local[1]][4] * As[kmod][tidx.local[0]][4] +
+                Bs[kmod][tidx.local[1]][5] * As[kmod][tidx.local[0]][5] +
+                Bs[kmod][tidx.local[1]][6] * As[kmod][tidx.local[0]][6] +
+                Bs[kmod][tidx.local[1]][7] * As[kmod][tidx.local[0]][7] +
+                Bs[kmod][tidx.local[1]][8] * As[kmod][tidx.local[0]][8] +
+                Bs[kmod][tidx.local[1]][9] * As[kmod][tidx.local[0]][9] +
+                Bs[kmod][tidx.local[1]][10] * As[kmod][tidx.local[0]][10] +
+                Bs[kmod][tidx.local[1]][11] * As[kmod][tidx.local[0]][11] +
+                Bs[kmod][tidx.local[1]][12] * As[kmod][tidx.local[0]][12] +
+                Bs[kmod][tidx.local[1]][13] * As[kmod][tidx.local[0]][13] +
+                Bs[kmod][tidx.local[1]][14] * As[kmod][tidx.local[0]][14] +
+                Bs[kmod][tidx.local[1]][15] * As[kmod][tidx.local[0]][15];
+
+   tidx.barrier.wait();
+   }
+   if (tidx.global[0] < M && tidx.global[1] < N)
+   {
+    Cmat[cOffset + tidx.global[1]][tidx.global[0]] *= beta;
+    Cmat[cOffset + tidx.global[1]][tidx.global[0]] += CValue * alpha;
+   }
+ });
+}
+
 #endif
 #if LOOPUNROLL
 static void gemm_NoTransAB_loopunroll(Concurrency::array_view<float, 1> &A, long aOffset,
