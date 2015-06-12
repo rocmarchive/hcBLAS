6bf82d871e2d3cfa5f9fa8ada78fb94d6250c097
Sujitha <sujitha@multicorewareinc.com>
2015-06-12 16:09:53 +0530

    Add Logic for saxpy targetting bigger sizes

--- source/saxpy.cpp
+++ source/saxpy.cpp
@@ -2,23 +2,56 @@
 #include <amp.h>
 
 using namespace concurrency;
-#define BLOCK_SIZE 16 
+#define BLOCK_SIZE 256 
 
 void axpy_AMP(long n, float alpha,
               Concurrency::array_view<float> &X, long xOffset, long incx,
               Concurrency::array_view<float> &Y, long yOffset, long incy)
 {
+  if(n <= 20480)
+  {
     long size = (n + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
     Concurrency::extent<1> compute_domain(size);
-
-    Concurrency::parallel_for_each(compute_domain.tile<BLOCK_SIZE>(),[=] 
-                                   (Concurrency::tiled_index<BLOCK_SIZE> tidx)
-                                   restrict(amp) {
-        if (tidx.global[0] < n)
-            Y[yOffset + tidx.global[0]] += X[xOffset + tidx.global[0]] * alpha;
+    Concurrency::parallel_for_each(compute_domain.tile<BLOCK_SIZE>(),[=] (Concurrency::tiled_index<BLOCK_SIZE> tidx) restrict(amp)
+    {
+      if(tidx.global[0] < n)
+        Y[yOffset + tidx.global[0]] += X[xOffset + tidx.global[0]] * alpha;
     });
-}
+  }
+  else
+  {
+    int step_sz;
+    if (n <= 409600)
+      step_sz = 3;
+    else if ( n <= 1939526)
+      step_sz = 7;
+    else
+      step_sz = 15;
 
+    long size = (n/step_sz + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
+    long nBlocks = size/BLOCK_SIZE;
+    Concurrency::extent<1> compute_domain(size);
+    Concurrency::parallel_for_each(compute_domain.tile<BLOCK_SIZE>(),[=] (Concurrency::tiled_index<BLOCK_SIZE> tidx) restrict(amp)
+    {
+      if(tidx.tile[0] != nBlocks -1)
+      {
+        for(int iter = 0; iter < step_sz; iter++)
+        {
+          Y[yOffset + tidx.tile[0] * 256 * step_sz + tidx.local[0] + iter*256] += X[xOffset + tidx.tile[0] * 256 * step_sz + tidx.local[0] + iter*256] * alpha;
+        }
+      }
+      else
+      {
+        int n_iter = ((n - ((nBlocks - 1) * 256 * step_sz)) / 256) + 1;
+        for(int iter = 0; iter < n_iter; iter++)
+        {
+          if (((nBlocks - 1) * 256 * step_sz + iter * 256 + tidx.local[0]) < n)
+            Y[yOffset + tidx.tile[0] * 256 * step_sz + tidx.local[0] + iter*256] += X[xOffset + tidx.tile[0] * 256 * step_sz + tidx.local[0] + iter*256] * alpha;
+        }
+      }
+    });
+  } 
+}
 ampblasStatus Ampblaslibrary :: ampblas_saxpy(const int N, const float *alpha,
                                               float *X, const int incX,
                                               float *Y, const int incY,
