9f885427b47b60515f54fc1404b67367decfe33d
aparnasuresh <aparna@multicorewareinc.com>
2015-07-08 19:16:31 +0530

    Add overloaded function to SGEMV routine for Batch processing

--- source/sgemv.cpp
+++ source/sgemv.cpp
@@ -5,7 +5,7 @@
 using namespace concurrency;
 
 static void gemv_TransA(Concurrency::accelerator_view &accl_view,
-			Concurrency::array_view<float> &A_mat, int aOffset,
+			Concurrency::array_view<float> &A_mat, long aOffset,
                         Concurrency::array_view<float> &X_vec, long xOffset,
                         Concurrency::array_view<float> &Y_vec, long yOffset,
                         float alpha, float beta, int lenX, int lenY,
@@ -104,10 +104,113 @@ static void gemv_TransA(Concurrency::accelerator_view &accl_view,
   }
 }
 
+static void gemv_TransA(Concurrency::accelerator_view &accl_view,
+			Concurrency::array_view<float> &A_mat, long aOffset, long A_batchOffset,
+                        Concurrency::array_view<float> &X_vec, long xOffset, long X_batchOffset,
+                        Concurrency::array_view<float> &Y_vec, long yOffset, long Y_batchOffset,
+                        float alpha, float beta, int lenX, int lenY,
+                        Concurrency::array_view<float> &tempBuf, int batchSize)
+{
+  if((lenX - lenY) > 5000)
+  {
+    int len_X = (lenX + (BLOCK_SIZE - 1)) & ~(BLOCK_SIZE - 1);
+    int num_blocks = len_X / BLOCK_SIZE;
+    Concurrency::extent<2> grdExt(batchSize, len_X);
+    Concurrency::tiled_extent<1,BLOCK_SIZE> t_ext(grdExt);
+
+    Concurrency::parallel_for_each(accl_view, t_ext,[=] (Concurrency::tiled_index<1, BLOCK_SIZE> tidx) restrict(amp)
+    {
+      tile_static float t[BLOCK_SIZE];
+      int elt = tidx.tile[0];
+      for (int Col = 0; Col < lenY; Col++)
+      {
+        int blockIdx = tidx.tile[1];
+        int threadIdx = tidx.local[1];
+        tempBuf[Col * num_blocks + blockIdx] = 0;
+        t[threadIdx] = 0;
+
+        if (Col < lenY && blockIdx * BLOCK_SIZE + threadIdx < lenX)
+          t[threadIdx] = X_vec[xOffset + X_batchOffset * elt + blockIdx * BLOCK_SIZE + threadIdx] * A_mat[aOffset + A_batchOffset * elt + Col * lenX + blockIdx * BLOCK_SIZE + threadIdx];
+
+        tidx.barrier.wait();
+
+        for (int stride = BLOCK_SIZE / 2; stride >= 1; stride /= 2)
+        {
+          if(threadIdx < stride)
+            t[threadIdx] += t[threadIdx + stride];
+        }
+        tempBuf[Col * num_blocks + blockIdx] = t[0];
+        tidx.barrier.wait();
+      }
+
+      if (tidx.tile[1] == 0)
+      {
+        for(int Col = 0; Col < lenY; Col++)
+        {
+          tile_static float sh[BLOCK_SIZE];
+          int threadId = tidx.local[1];
+          sh[tidx.local[1]] = 0;
+
+          for (int i = threadId; i < num_blocks; i += tidx.tile_dim0)
+            sh[threadId] += tempBuf[Col * num_blocks + i];
+
+          tidx.barrier.wait();
+
+          for (int stride = BLOCK_SIZE / 2; stride >= 1; stride /= 2)
+          {
+            if(threadId < stride)
+              sh[threadId] += sh[threadId + stride];
+          }
+          tidx.barrier.wait();
+          Y_vec[yOffset + Y_batchOffset * elt + Col] *= beta;
+          Y_vec[yOffset + Y_batchOffset * elt + Col] += alpha * sh[0];
+        }
+      }
+    });
+  }
+  else
+  {
+    Concurrency::extent<2> grdExt(batchSize, lenY * BLOCK_SIZE);
+    Concurrency::tiled_extent<1, BLOCK_SIZE> t_ext(grdExt);
+
+    Concurrency::parallel_for_each(accl_view, t_ext, [=] (Concurrency::tiled_index<1, BLOCK_SIZE> tidx) restrict(amp)
+    {
+      int elt = tidx.tile[0];
+      int threadIdx = tidx.local[1];
+      int blockIdx = tidx.tile[1];
+      int Col = blockIdx;
+
+      tile_static float sh[BLOCK_SIZE];
+      sh[threadIdx] = 0;
+
+      for (int tileId = 0; tileId < ((lenX + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1)) / BLOCK_SIZE; tileId++)
+      {
+        if (tileId * BLOCK_SIZE + threadIdx < lenX && Col < lenY)
+          sh[threadIdx] += X_vec[xOffset + X_batchOffset * elt + tileId * BLOCK_SIZE + threadIdx] * A_mat[aOffset + A_batchOffset * elt + Col * lenX + tileId * BLOCK_SIZE + threadIdx];
+      }
+      tidx.barrier.wait();
+
+      for (int stride = BLOCK_SIZE / 2; stride >= 1; stride /= 2)
+      {
+        if (threadIdx < stride)
+          sh[threadIdx] += sh[threadIdx + stride];
+        tidx.barrier.wait();
+      }
+
+      if(threadIdx == 0 && Col < lenY)
+      {
+        Y_vec[yOffset + Y_batchOffset * elt + Col] *= beta;
+        Y_vec[yOffset + Y_batchOffset * elt + Col] += alpha * sh[0];
+      }
+    });
+  }
+}
+
+#define TILE_SZ_A 16
 #define TILE_SZ_A 16
 
 static void gemv_TransA_register(Concurrency::accelerator_view &accl_view,
-				 Concurrency::array_view<float> &A_mat, int aOffset,
+				 Concurrency::array_view<float> &A_mat, long aOffset,
                                  Concurrency::array_view<float> &X_vec, long xOffset,
                                  Concurrency::array_view<float> &Y_vec, long yOffset,
                                  float alpha, float beta, int lenX, int lenY,
@@ -224,7 +327,7 @@ static void gemv_NoTransA_register(Concurrency::accelerator_view &accl_view,Conc
 
 
 static void gemv_NoTransA(Concurrency::accelerator_view &accl_view,
-			  Concurrency::array_view<float> &A, long aOffset,
+			  Concurrency::array_view<float> &A, long aOffset, 
                           Concurrency::array_view<float> &X, long xOffset,
                           Concurrency::array_view<float> &Y, long yOffset,
                           float alpha, float beta, int lenX, int lenY)
@@ -264,6 +367,48 @@ static void gemv_NoTransA(Concurrency::accelerator_view &accl_view,
   });
 }
 
+static void gemv_NoTransA(Concurrency::accelerator_view &accl_view,
+			  Concurrency::array_view<float> &A, long aOffset, long A_batchOffset,
+                          Concurrency::array_view<float> &X, long xOffset, long X_batchOffset,
+                          Concurrency::array_view<float> &Y, long yOffset, long Y_batchOffset,
+                          float alpha, float beta, int lenX, int lenY, int batchSize)
+{
+  long size = (lenY + 255) & ~255;
+  Concurrency::extent<2> compute_domain(batchSize, size);
+
+  Concurrency::parallel_for_each(accl_view, compute_domain.tile<1, BLOCK_SIZE>(),[=] (Concurrency::tiled_index<1, BLOCK_SIZE> tidx) restrict(amp)
+  {
+    int elt = tidx.tile[0];
+    int bx = tidx.tile[1];
+    int tx = tidx.local[1];
+    tile_static float Xds[BLOCK_SIZE];
+    int Col = bx * BLOCK_SIZE + tx;
+    float Pvalue = 0;
+
+    for (int m = 0; m < (lenX - 1) / BLOCK_SIZE + 1; ++m)
+    {
+      if (m * BLOCK_SIZE + tx < lenX)
+        Xds[tx] = X[xOffset + X_batchOffset * elt + m * BLOCK_SIZE + tx];
+      else
+        Xds[tx]=0;
+
+      tidx.barrier.wait();
+
+      for (int k = 0; k < BLOCK_SIZE; k++)
+        if (Col < lenY && m * BLOCK_SIZE + k < lenX)
+          Pvalue += Xds[k] * A[aOffset + A_batchOffset * elt + Col + (m * BLOCK_SIZE + k) * lenY];
+
+      tidx.barrier.wait();
+    }
+    if (Col < lenY)
+    {
+      Y[yOffset + Y_batchOffset * elt + Col] *= beta;
+      Y[yOffset + Y_batchOffset * elt + Col] += alpha * Pvalue;
+    }
+    tidx.barrier.wait();
+  });
+}
+
 void gemv_AMP(Concurrency::accelerator_view &accl_view,
               char TransA, int M, int N, float alpha,
               Concurrency::array_view<float> &A, long aOffset,
@@ -295,6 +440,41 @@ void gemv_AMP(Concurrency::accelerator_view &accl_view,
   else if (TransA == 'n')
     gemv_NoTransA(accl_view, A, aOffset, X, xOffset, Y, yOffset, alpha, beta, lenX, lenY);
 }
+
+void gemv_AMP(Concurrency::accelerator_view &accl_view,
+              char TransA, int M, int N, float alpha,
+              Concurrency::array_view<float> &A, long aOffset, long A_batchOffset,
+              Concurrency::array_view<float> &X, long xOffset, long X_batchOffset, 
+              long incX, float beta,
+              Concurrency::array_view<float> &Y, long yOffset, long Y_batchOffset,
+              long incY, Concurrency::array_view<float> &temp_buf, int batchSize)
+{
+
+  int lenX, lenY;
+  if (M == 0 || N == 0)
+    return;
+
+  if (alpha == 0.0 && beta == 1.0)
+    return;
+
+  if (TransA == 'n')
+  {
+    lenX = N;
+    lenY = M;
+  }
+  else
+  {
+    lenX = M;
+    lenY = N;
+  }
+
+  if (TransA == 't')
+    gemv_TransA(accl_view, A, aOffset, A_batchOffset, X, xOffset, X_batchOffset, Y, yOffset, Y_batchOffset, alpha, beta, lenX, lenY, temp_buf, batchSize);
+  else if (TransA == 'n')
+    gemv_NoTransA(accl_view, A, aOffset, A_batchOffset, X, xOffset, X_batchOffset, Y, yOffset, Y_batchOffset, alpha, beta, lenX, lenY, batchSize);
+}
+
+
 
 ampblasStatus Ampblaslibrary :: ampblas_sgemv(const enum AMPBLAS_TRANS type,
                                               const int M, const int N,
@@ -376,4 +556,34 @@ ampblasStatus Ampblaslibrary :: ampblas_sgemv(Concurrency::accelerator_view &acc
 
     return AMPBLAS_SUCCESS;
 }
+
+ampblasStatus Ampblaslibrary :: ampblas_sgemv(Concurrency::accelerator_view &accl_view,
+                                              const enum AMPBLAS_TRANS type, const int M,
+                                              const int N, const float &alpha, Concurrency::array_view<float> &A,
+                                              const long aOffset, const long A_batchOffset, const int lda,
+                                              Concurrency::array_view<float> &X, 
+                                              const long xOffset, const long X_batchOffset, const int incX,
+                                              const float &beta, Concurrency::array_view<float> &Y, 
+                                              const long yOffset, const long Y_batchOffset, const int incY, const int batchSize)
+{
+  /*Check the conditions*/
+  if(M <= 0 || N <= 0)
+        return AMPBLAS_INVALID;
+
+  if( alpha == 0 && beta == 1)
+        return AMPBLAS_SUCCESS;
+
+    long lenXt = 1 + (M - 1) * abs(incX);
+    long lenYt = 1 + (N - 1) * abs(incY);
+
+    int num_blocks = lenXt / BLOCK_SIZE;
+    float* temp = (float*)malloc(num_blocks * lenYt * sizeof(float));
+    Concurrency::array_view<float> tempBuf(num_blocks * lenYt, temp);
+
+    gemv_AMP(accl_view, type, M, N, alpha, A, aOffset, A_batchOffset, X, xOffset, X_batchOffset, incX, beta, Y, yOffset, Y_batchOffset, incY, tempBuf, batchSize);
+    A.synchronize();
+
+    return AMPBLAS_SUCCESS;
+}
+
 
