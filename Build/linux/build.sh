62867126406b90acd3f38d486dbe7d54cd2bf572
Neelakandan <neelakandan@multicorewareinc.com>
2015-07-21 23:14:57 -0500

    Modify build script to reflect wiki changes

--- Build/linux/build.sh
+++ Build/linux/build.sh
@@ -1,4 +1,4 @@
 
   
 
-cmake ../../source -DCMAKE_C_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPROOT}/gmac_exp_build_cache/compiler/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
+cmake ../../source -DCMAKE_C_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
