a768eebadc3f495bc7c5b53cc34b6c6effe60398
aparnasuresh <aparna@multicorewareinc.com>
2015-11-02 16:14:37 +0530

    Change cmake to support debian package

--- Build/linux/build.sh
+++ Build/linux/build.sh
@@ -1,7 +1,7 @@
-CLANG_PATH="/opt/clamp/bin/clang++"
+CLANG_PATH="/opt/kalmar/bin/clang++"
 
 if [ -x $CLANG_PATH ]; then
-       cmake ../../source -DCMAKE_C_COMPILER=/opt/clamp/bin/clang  -DCMAKE_CXX_COMPILER=/opt/clamp/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
+       cmake ../../source -DCMAKE_C_COMPILER=/opt/kalmar/bin/clang  -DCMAKE_CXX_COMPILER=/opt/kalmar/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
 else
        cmake ../../source -DCMAKE_C_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
 fi
