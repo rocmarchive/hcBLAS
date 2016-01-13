61f10d508f0d31f9d867f72ce7122c3424ffc22a
Neelakandan <neelakandan@multicorewareinc.com>
2016-01-13 18:57:21 +0530

    Rename to HCBLAS

--- lib/include/hcblas.h
+++ lib/include/hcblas.h
@@ -3,18 +3,18 @@
 
 // Type hcblasStatus
 
-// The type  hcblasStatus  is used for function status returns. CUBLAS 
-// helper functions return status directly, while the status of CUBLAS 
+// The type  hcblasStatus  is used for function status returns. HCBLAS 
+// helper functions return status directly, while the status of HCBLAS 
 // core functions can be retrieved via  hcblasGetError() . Currently, the 
 // following values are defined: 
 
 typedef enum hcblasStatus_t {
-  CUBLAS_STATUS_NOT_INITIALIZED  // CUBLAS library not initialized
-  CUBLAS_STATUS_ALLOC_FAILED     // resource allocation failed
-  CUBLAS_STATUS_INVALID_VALUE    // unsupported numerical value was passed to function
-  CUBLAS_STATUS_MAPPING_ERROR    // access to GPU memory space failed
-  CUBLAS_STATUS_EXECUTION_FAILED // GPU program failed to execute
-  CUBLAS_STATUS_INTERNAL_ERROR   // an internal CUBLAS operation failed
+  HCBLAS_STATUS_NOT_INITIALIZED  // HCBLAS library not initialized
+  HCBLAS_STATUS_ALLOC_FAILED     // resource allocation failed
+  HCBLAS_STATUS_INVALID_VALUE    // unsupported numerical value was passed to function
+  HCBLAS_STATUS_MAPPING_ERROR    // access to GPU memory space failed
+  HCBLAS_STATUS_EXECUTION_FAILED // GPU program failed to execute
+  HCBLAS_STATUS_INTERNAL_ERROR   // an internal HCBLAS operation failed
 } hcblasStatus;
 
 
