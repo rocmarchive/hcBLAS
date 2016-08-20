d681c3667e42501973d587aa96965fef6ce9c2d2
Neelakandan <neelakandan@multicorewareinc.com>
2016-08-20 10:43:30 +0530

    Add L2 normalization for floating point comparision

---
+++ test/src/helper_functions.h
@@ -0,0 +1,83 @@
+#ifndef HELPER_FUNC_H
+#define HELPER_FUNC_H
+
+#include <iostream>
+#include <math.h>
+
+bool sgemmCompareL2fe(const float *reference, const float *data,
+               const unsigned int len, const float epsilon)
+{
+    if(epsilon < 0) {
+      std::cout<<"Invalid epsilon value"<<std::endl;
+    }
+
+    float error = 0;
+    float ref = 0;
+
+    for (unsigned int i = 0; i < len; ++i)
+    {
+
+        float diff = reference[i] - data[i];
+        error += diff * diff;
+        ref += reference[i] * reference[i];
+    }
+
+    float normRef = sqrtf(ref);
+
+    if (fabs(ref) < 1e-7)
+    {
+#ifdef _DEBUG
+        std::cerr << "ERROR, reference l2-norm is 0\n";
+#endif
+        return false;
+    }
+
+    float normError = sqrtf(error);
+    error = normError / normRef;
+    bool result = error < epsilon;
+#ifdef _DEBUG
+
+    if (! result)
+    {
+        std::cerr << "ERROR, l2-norm error "
+                  << error << " is greater than epsilon " << epsilon << "\n";
+    }
+
+#endif
+
+    return result;
+}
+
+void printDiff(float *data1, float *data2, int width, int height, int iListLength, float fListTol)
+{
+    printf("Listing first %d Differences > %.6f...\n", iListLength, fListTol);
+    int i,j,k;
+    int error_count=0;
+
+    for (j = 0; j < height; j++)
+    {
+        if (error_count < iListLength)
+        {
+            printf("\n  Row %d:\n", j);
+        }
+
+        for (i = 0; i < width; i++)
+        {
+            k = j * width + i;
+            float fDiff = fabs(data1[k] - data2[k]);
+
+            if (fDiff > fListTol)
+            {
+                if (error_count < iListLength)
+                {
+                    printf("    Loc(%d,%d)\tCPU=%.5f\tGPU=%.5f\tDiff=%.6f\n", i, j, data1[k], data2[k], fDiff);
+                }
+
+                error_count++;
+            }
+        }
+    }
+
+    printf(" \n  Total Errors = %d\n", error_count);
+}
+#endif
