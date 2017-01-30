13a8e628d4c769daa8b2439c191d8c7b622606d6
Neelakandan <neelakandan@multicorewareinc.com>
2017-01-30 08:00:45 +0000

    Fix: hcblasGetAcclView Prototype in implementation

--- lib/src/hcblas.cpp
+++ lib/src/hcblas.cpp
@@ -182,11 +182,11 @@ hcblasStatus_t hcblasSetAcclView(hcblasHandle_t handle, hc::accelerator_view acc
 // HCBLAS_STATUS_SUCCESS : the stream was returned successfully
 // HCBLAS_STATUS_NOT_INITIALIZED : the library was not initialized
 
-hcblasStatus_t  hcblasGetAcclView(hcblasHandle_t handle, hc::accelerator_view **accl_view, void** stream) {
+hcblasStatus_t  hcblasGetAcclView(hcblasHandle_t handle, hc::accelerator_view *accl_view, void** stream) {
   if (handle == nullptr) {
     return HCBLAS_STATUS_NOT_INITIALIZED;
   }
-  *accl_view = &handle->currentAcclView;
+  accl_view = &handle->currentAcclView;
   *stream = handle->currentStream;
   return HCBLAS_STATUS_SUCCESS;
 }
