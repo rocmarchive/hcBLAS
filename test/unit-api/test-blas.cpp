65fa8b72e941737be16a5d18a1ec4bfec44d49db
Yuvaraj Venkatesh <yuvaraj@multicorewareinc.com>
2017-03-07 10:39:31 +0000

    Add hc gtest for cscal

--- test/unit-api/test-blas.cpp
+++ test/unit-api/test-blas.cpp
@@ -348,6 +348,110 @@ TEST(hcblaswrapper_dscalBatched, func_return_correct_dscalBatched) {
   hc::am_free(devX);
 }
 
+TEST(hcblaswrapper_cscal, func_return_correct_cscal) {
+  hcblasStatus_t status;
+  hcblasHandle_t handle = NULL;
+  hc::accelerator default_acc;
+  hc::accelerator_view av=default_acc.get_default_view();
+  // Passing a Null handle and default accelerator to the API
+  status = hcblasCreate(&handle, &av);
+  int n = 123;
+  int incx = 1;
+  long lenx = 1 + (n-1) * abs(incx);
+  float alpha[2];
+  hcComplex cAlpha;
+
+  cAlpha.x = 1;
+  cAlpha.y = 1;
+  alpha[0] = cAlpha.x;
+  alpha[1] = cAlpha.y;
+
+  // HCBLAS_STATUS_SUCCESS and FUNCTIONALITY CHECK
+  float *Xcblas = (float*)calloc(lenx * 2, sizeof(float));
+  hcComplex *X = (hcComplex*)calloc(lenx, sizeof(hcComplex));//host input
+  hcComplex* devX = hc::am_alloc(sizeof(hcComplex) * lenx, handle->currentAccl, 0);
+  int k = 0;
+  for(int i = 0; i < lenx; i++){
+            X[i].x = rand() % 10;
+            X[i].y = rand() % 20;
+            Xcblas[k++] = X[i].x;
+            Xcblas[k++] = X[i].y;
+  }
+  status = hcblasSetVector(handle, lenx, sizeof(hcComplex), X, incx, devX, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  status = hcblasCscal(handle, n, &cAlpha, devX, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  status = hcblasGetVector(handle, lenx, sizeof(hcComplex), devX, incx, X, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  cblas_cscal( n, alpha, Xcblas, incx );
+  for(int i = 0, k = 0; i < lenx && k < lenx * 2 ; i++, k = k + 2){
+        EXPECT_EQ(X[i].x, Xcblas[k]);
+        EXPECT_EQ(X[i].y, Xcblas[k+1]);
+  }
+
+  // HCBLAS_STATUS_NOT_INITIALIZED
+  hcblasDestroy(&handle);
+  status = hcblasCscal(handle, n, &cAlpha, devX, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_NOT_INITIALIZED);
+
+  free(X);
+  free(Xcblas);
+  hc::am_free(devX);
+}
+
+TEST(hcblaswrapper_cscalBatched, func_return_correct_cscalBatched) {
+  hcblasStatus_t status;
+  hcblasHandle_t handle = NULL;
+  hc::accelerator default_acc;
+  hc::accelerator_view av=default_acc.get_default_view();
+  // Passing a Null handle and default accelerator to the API
+  status = hcblasCreate(&handle, &av);
+  int n = 123;
+  int incx = 1;
+  long lenx = 1 + (n-1) * abs(incx);
+  float alpha[2];
+  int batchSize = 128;
+  hcComplex cAlpha;
+
+  cAlpha.x = 1;
+  cAlpha.y = 1;
+  alpha[0] = cAlpha.x;
+  alpha[1] = cAlpha.y;
+
+  // HCBLAS_STATUS_SUCCESS and FUNCTIONALITY CHECK
+  float *Xcblas = (float*)calloc(lenx * batchSize * 2 , sizeof(float));
+  hcComplex *X = (hcComplex*)calloc(lenx * batchSize, sizeof(hcComplex));//host input
+  hcComplex* devX = hc::am_alloc(sizeof(hcComplex) * lenx * batchSize, handle->currentAccl, 0);
+  int k = 0;
+  for(int i = 0; i < lenx * batchSize; i++){
+            X[i].x = rand() % 10;
+            X[i].y = rand() % 20;
+            Xcblas[k++] =  X[i].x;
+            Xcblas[k++] =  X[i].y;
+  }
+  status = hcblasSetVector(handle, lenx*batchSize, sizeof(hcComplex), X, incx, devX, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  status = hcblasCscalBatched(handle, n, &cAlpha, devX, incx, batchSize);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  status = hcblasGetVector(handle, lenx*batchSize, sizeof(hcComplex), devX, incx, X, incx);
+  EXPECT_EQ(status, HCBLAS_STATUS_SUCCESS);
+  for(int i = 0; i < batchSize; i++)
+          cblas_cscal( n, alpha, Xcblas + i * n * 2, incx);
+  for(int i =0, k = 0; i < lenx * batchSize && k < lenx * batchSize * 2; i++, k = k + 2){
+          EXPECT_EQ(X[i].x, Xcblas[k]);
+          EXPECT_EQ(X[i].y, Xcblas[k+1]);
+  }
+
+  // HCBLAS_STATUS_NOT_INITIALIZED
+  hcblasDestroy(&handle);
+  status = hcblasCscalBatched(handle, n, &cAlpha, devX, incx, batchSize);
+  EXPECT_EQ(status, HCBLAS_STATUS_NOT_INITIALIZED);
+
+  free(X);
+  free(Xcblas);
+  hc::am_free(devX);
+}
+
 TEST(hcblaswrapper_scopy, func_return_correct_scopy) {
   hcblasStatus_t status;
   hcblasHandle_t handle = NULL;
