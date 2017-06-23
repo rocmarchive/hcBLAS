15df618565910ca8b1cc0090fc68e203ff0fe91b
Santhosh Kumar Rajamanickam <santhosh@multicorewareinc.com>
2017-06-23 14:27:40 +0000

    Fix symlink creation bug
    - Fixed symlink bug. Symlink libhipblas.so will be created under
      /opt/rocm/hcblas/lib and /opt/rocm/lib when --install is enabled.
    - Add support for --deepclean which cleans files under /opt/rocm/hcblas
      when enables during running ./clean.sh
    
    hcblas: cleanup-symlink-bug

--- clean.sh
+++ clean.sh
@@ -4,7 +4,49 @@
 # CURRENT_WORK_DIRECTORY
 current_work_dir=$PWD
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+install=0
+
+# Help menu
+print_help() {
+cat <<-HELP
+=============================================================================================================================
+Please provide the following arguments:
+
+  ${green}--deepclean${reset}  To clean the files under /opt/rocm/hcblas  Requires sudo perms.
+
+=============================================================================================================================
+Usage: ./clean.sh --deepclean 
+=============================================================================================================================
+HELP
+exit 0
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --deepclean)
+      deepclean="1"
+      ;;
+    --help) print_help;;
+    *)
+      printf "************************************************************\n"
+      printf "* Error: Invalid arguments, run --help for valid arguments.*\n"
+      printf "************************************************************\n"
+      exit 1
+  esac
+  shift
+done
+
 # Remove build
 rm -rf $current_work_dir/build
+
+# Remove files under /opt/rocm/hcrng
+if [ "$deepclean" = "1" ]; then
+  sudo rm -rf /opt/rocm/hcblas
+  sudo rm -f /opt/rocm/lib/libhcblas.so
+  sudo rm -f /opt/rocm/lib/libhipblas.so
+fi
 
 
