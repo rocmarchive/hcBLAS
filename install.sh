0438c6d9c50491315ae765a80bd365a26cc2f006
Neelakandan <neelakandan@multicorewareinc.com>
2017-08-03 12:30:49 +0530

    Add Preliminary install and uninstall script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# bash script to install the library 
+
+#invoke build script with install argument 
+
+#also do a preliminary basic testing
+
+sh build.sh --test=basic --install
+
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+# bash script to uninstall the library
+
+sh clean.sh --deepclean
+echo "Uninstallation of hcBLAS done"
+
+
